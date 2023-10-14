f9ce2156e1ab2b7c722aad0b7ab24610ec110c54
realdiegor <diegoreyes150103@gmail.com>
2023-10-13 22:47:49 -0400

    refactor: modularize settings routes in its own file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,8 @@
 import { HomePage } from '@/pages/home';
 import { NotFoundPage } from '@/pages/not-found';
-import { SettingsPage } from '@/pages/settings';
-import { ParametersPage } from '@/pages/settings/system';
-import { UsersPage } from '@/pages/settings/system/users';
-import { NewUsersPage } from '@/pages/settings/system/users/new-user';
-import { HolidaysPage } from '@/pages/settings/tables/holidays';
-import { NewHolidaysPage } from '@/pages/settings/tables/holidays/new-holiday';
-import { SpecialtiesPage } from '@/pages/settings/tables/specialties';
-import { NewSpecialtiesPage } from '@/pages/settings/tables/specialties/new-specialty';
 import { createBrowserRouter } from 'react-router-dom';
 import { MainLayout } from '@/layouts/main-layout';
+import { settingsRouter } from './settings';
 
 export const router = createBrowserRouter([
 	{
@@ -20,38 +13,7 @@ export const router = createBrowserRouter([
 				index: true,
 				element: <HomePage />,
 			},
-			{
-				path: '/configuracion',
-				element: <SettingsPage />,
-			},
-			{
-				path: '/configuracion/parametros',
-				element: <ParametersPage />,
-			},
-			{
-				path: '/configuracion/usuarios',
-				element: <UsersPage />,
-			},
-			{
-				path: '/configuracion/usuarios/nuevo',
-				element: <NewUsersPage />,
-			},
-			{
-				path: '/configuracion/especialidades',
-				element: <SpecialtiesPage />,
-			},
-			{
-				path: '/configuracion/especialidades/nuevo',
-				element: <NewSpecialtiesPage />,
-			},
-			{
-				path: '/configuracion/feriados',
-				element: <HolidaysPage />,
-			},
-			{
-				path: '/configuracion/feriados/nuevo',
-				element: <NewHolidaysPage />,
-			},
+			...settingsRouter,
 			{
 				path: '*',
 				element: <NotFoundPage />,
---
+++ src/routes/settings/index.jsx
@@ -0,0 +1,43 @@
+import { SettingsPage } from '@/pages/settings';
+import { ParametersPage } from '@/pages/settings/system';
+import { UsersPage } from '@/pages/settings/system/users';
+import { NewUsersPage } from '@/pages/settings/system/users/new-user';
+import { HolidaysPage } from '@/pages/settings/tables/holidays';
+import { NewHolidaysPage } from '@/pages/settings/tables/holidays/new-holiday';
+import { SpecialtiesPage } from '@/pages/settings/tables/specialties';
+import { NewSpecialtiesPage } from '@/pages/settings/tables/specialties/new-specialty';
+
+export const settingsRouter = [
+	{
+		path: '/configuracion',
+		element: <SettingsPage />,
+	},
+	{
+		path: '/configuracion/parametros',
+		element: <ParametersPage />,
+	},
+	{
+		path: '/configuracion/usuarios',
+		element: <UsersPage />,
+	},
+	{
+		path: '/configuracion/usuarios/nuevo',
+		element: <NewUsersPage />,
+	},
+	{
+		path: '/configuracion/especialidades',
+		element: <SpecialtiesPage />,
+	},
+	{
+		path: '/configuracion/especialidades/nuevo',
+		element: <NewSpecialtiesPage />,
+	},
+	{
+		path: '/configuracion/feriados',
+		element: <HolidaysPage />,
+	},
+	{
+		path: '/configuracion/feriados/nuevo',
+		element: <NewHolidaysPage />,
+	},
+];

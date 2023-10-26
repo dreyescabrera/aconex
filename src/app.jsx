adc9c09e3a903031fe133e22f11f564a262d3364
Martin-M42 <83964814+Martin-M42@users.noreply.github.com>
2023-10-26 17:20:19 -0300

    endpoint to settings/table/specialties added (#5)
    * endpoint to settings/table/specialties added
    
    * input deleted, backwards button added

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,25 @@
 import { router } from '@/routes';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'dayjs/locale/es';
 import { Helmet } from 'react-helmet';
 import { RouterProvider } from 'react-router-dom';
 
+const queryClient = new QueryClient();
+
 export function App() {
 	return (
 		<>
-			<Helmet
-				titleTemplate="%s | AConex - Boot & Calendar"
-				defaultTitle="AConex - Boot & Calendar"
-			/>
-			<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
-				<RouterProvider router={router} />
-			</LocalizationProvider>
+			<QueryClientProvider client={queryClient}>
+				<Helmet
+					titleTemplate="%s | AConex - Boot & Calendar"
+					defaultTitle="AConex - Boot & Calendar"
+				/>
+				<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+					<RouterProvider router={router} />
+				</LocalizationProvider>
+			</QueryClientProvider>
 		</>
 	);
 }
--- src/pages/settings/tables/specialties/new-specialty/new-specialty-page.jsx
+++ src/pages/settings/tables/specialties/new-specialty/new-specialty-page.jsx
@@ -2,10 +2,42 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { useMutation } from '@tanstack/react-query';
 import { Helmet } from 'react-helmet';
 import { Form, TextInput } from '@/components/form';
+import { api } from '@/services/api';
+
+async function addspecialty(specialty) {
+	const res = await api.post('/especialidades', specialty);
+	return res;
+}
+
+const Message = ({ status }) => {
+	if (status.isLoading) {
+		return <p>Cargando...</p>;
+	}
+	if (status.isSuccess) {
+		return (
+			<div>
+				<p>Especialidad agregada con exito</p>
+				<Button variant="contained" sx={{ mt: 'auto' }} href="../">
+					Volver
+				</Button>
+			</div>
+		);
+	}
+	if (status.isError) {
+		return <p>Error al agregar especialidad</p>;
+	}
+};
 
 export const NewSpecialtiesPage = () => {
+	const mutation = useMutation(addspecialty);
+	const handleSubmit = (event) => {
+		let specialty = { clinicaId: 1, nombre: event.descripcion }; //el Codigo de la clinica debe obtenerse desde los datos del usuario a traves del backend Arreglar esto
+		mutation.mutate(specialty);
+	};
+
 	return (
 		<>
 			<Helmet>
@@ -18,9 +50,8 @@ export const NewSpecialtiesPage = () => {
 				<Typography paragraph>
 					Especifica el código y descripción de la nueva especialidad
 				</Typography>
-				<Form onSubmit={console.info} defaultValues={{ codigo: '', descripcion: '' }}>
+				<Form onSubmit={handleSubmit} defaultValues={{ codigo: '', descripcion: '' }}>
 					<Stack direction="row" spacing={4} justifyContent="start">
-						<TextInput name="codigo" label="Código" variant="outlined" type="number" />
 						<TextInput
 							name="descripcion"
 							label="Descripción"
@@ -34,6 +65,7 @@ export const NewSpecialtiesPage = () => {
 						Agregar
 					</Button>
 				</Form>
+				<Message status={mutation} />
 			</Container>
 		</>
 	);

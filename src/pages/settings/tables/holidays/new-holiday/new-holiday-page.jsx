499ea34075156a7f8f2fdda5821edea64ae47a7e
Walter <martinguevara109@gmail.com>
2023-10-28 19:55:52 -0300

    endpoints added to holidays-page and new-holiday-page in the settings folder

--- src/pages/settings/tables/holidays/new-holiday/new-holiday-page.jsx
+++ src/pages/settings/tables/holidays/new-holiday/new-holiday-page.jsx
@@ -2,10 +2,42 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { useMutation } from '@tanstack/react-query';
 import { Helmet } from 'react-helmet';
 import { DatePicker, Form, TextInput } from '@/components/form';
+import { api } from '@/services/api';
+
+async function addholiday(holiday) {
+	const res = api.post('/feriados', holiday);
+	return res;
+}
+
+const Mensaje = ({ status }) => {
+	if (status.isLoading) {
+		return <p>Cargando...</p>;
+	}
+	if (status.isSuccess) {
+		return (
+			<div>
+				<p>Feriado Agregado con exito!</p>
+				<Button variant="contained" sx={{ mt: 'auto' }} href="../">
+					Volver
+				</Button>
+			</div>
+		);
+	}
+	if (status.isError) {
+		return <p>Error al agregar Feriado</p>;
+	}
+};
 
 export const NewHolidaysPage = () => {
+	const mutation = useMutation(addholiday);
+	const handleSubmit = (event) => {
+		var date = event.dia.format('MM/DD').toString();
+		let holiday = { clinicaId: 1, descripcion: event.descripcion, fecha: date }; //Es Necesario especificar la clinica que agregara tal feriado (clinicaId)
+		mutation.mutate(holiday);
+	};
 	return (
 		<>
 			<Helmet>
@@ -16,7 +48,7 @@ export const NewHolidaysPage = () => {
 					Nuevo Feriado
 				</Typography>
 				<Typography paragraph>Especifica el día y descripción del nuevo feriado</Typography>
-				<Form onSubmit={console.info} defaultValues={{ dia: null, descripcion: '' }}>
+				<Form onSubmit={handleSubmit} defaultValues={{ dia: null, descripcion: '' }}>
 					<Stack direction="row" spacing={4} justifyContent="start">
 						<DatePicker name="dia" label="Día" disablePast />
 						<TextInput
@@ -32,6 +64,7 @@ export const NewHolidaysPage = () => {
 						Agregar
 					</Button>
 				</Form>
+				<Mensaje status={mutation} />
 			</Container>
 		</>
 	);

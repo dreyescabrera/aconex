a8d3c40dfbffd2f1a7b7fa913c6fea21a9c60c66
realdiegor <diegoreyes150103@gmail.com>
2023-11-06 18:22:32 -0500

    feat(agenda): group shifts by professionals when not specified

---
+++ src/pages/agenda/components/shifts/multi-professional-shifts-table.jsx
@@ -0,0 +1,81 @@
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import { Fragment } from 'react';
+import { SkeletonRows } from './skeleton-rows';
+import { BodyRow } from './table/body-row';
+import { ContactCell } from './table/contact-cell';
+import { HeadRow } from './table/head-row';
+import { OptionsCell } from './table/options-cell';
+import { TimeCell } from './table/time-cell';
+
+/**
+ * @param {object} props
+ * @param {Array<any>} props.shiftsToRender
+ * @param {(shift: any, drawerToOpen: import('../../context/agenda.context').DrawerToOpen) => () => void} props.handleShiftOptions
+ */
+
+export const MultiProfessionalShiftsTable = ({ shiftsToRender, handleShiftOptions }) => {
+	const shiftsMatrix = Object.entries(shiftsToRender);
+
+	return (
+		<TableContainer component={Paper} sx={{ mt: 4 }}>
+			<Table>
+				<HeadRow />
+				<TableBody>
+					{typeof shiftsToRender === 'undefined' ? (
+						<SkeletonRows />
+					) : (
+						<>
+							{shiftsMatrix?.map(([professionalName, shifts]) => (
+								<Fragment key={professionalName}>
+									<TableRow>
+										<TableCell colSpan={7} sx={{ fontSize: 18 }}>
+											{professionalName}
+										</TableCell>
+									</TableRow>
+									{shifts.map((shift) => (
+										<>
+											{shift.pacienteId ? (
+												<BodyRow>
+													<TimeCell date={shift.date} />
+
+													<TableCell>
+														{shift.paciente.perfil.nombre} {shift.paciente.perfil.apellido}
+													</TableCell>
+													<TableCell>{shift.presentismo}</TableCell>
+													<TableCell>{shift.obraSocial}</TableCell>
+													<TableCell>{shift.observacion}</TableCell>
+													<ContactCell phoneNumber={shift.paciente.perfil.celular} />
+													<OptionsCell onClick={handleShiftOptions(shift, 'shiftOptions')} />
+												</BodyRow>
+											) : (
+												<TableRow
+													sx={{
+														cursor: 'pointer',
+														pointerEvents: shift.habilitado ? 'auto' : 'none',
+														'&:hover': { background: '#CCC2' },
+													}}
+													onClick={handleShiftOptions(shift, 'emptyShiftOptions')}
+												>
+													<TimeCell date={shift.date} />
+													<TableCell sx={{ fontStyle: 'italic' }} colSpan={5}>
+														{!shift.habilitado ? 'Deshabilitado' : ''}
+													</TableCell>
+													<OptionsCell disabled />
+												</TableRow>
+											)}
+										</>
+									))}
+								</Fragment>
+							))}
+						</>
+					)}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
--- src/pages/agenda/components/shifts/skeleton-rows.jsx
+++ src/pages/agenda/components/shifts/skeleton-rows.jsx
@@ -6,24 +6,17 @@ export const SkeletonRows = () => {
 	const totalRows = 12;
 	const iterable = new Array(totalRows).fill();
 
-	return (
-		<>
-			{iterable.map((_, i) => (
-				<TableRow key={i}>
-					<TableCell>
-						<Skeleton />
-					</TableCell>
-					<TableCell>
-						<Skeleton />
-					</TableCell>
-					<TableCell>
-						<Skeleton />
-					</TableCell>
-					<TableCell>
-						<Skeleton />
-					</TableCell>
-				</TableRow>
-			))}
-		</>
-	);
+	return iterable.map((_, i) => (
+		<TableRow key={i}>
+			<TableCell>
+				<Skeleton />
+			</TableCell>
+			<TableCell colSpan={3}>
+				<Skeleton />
+			</TableCell>
+			<TableCell colSpan={3}>
+				<Skeleton />
+			</TableCell>
+		</TableRow>
+	));
 };

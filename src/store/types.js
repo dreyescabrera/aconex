3459755cbd544a96a5f025c1177517dc81514bbb
LionelCMZ <moraleslionel792@gmail.com>
2024-08-14 23:22:41 -0400

    crear sobreturno, eliminar usuarios

--- src/store/types.js
+++ src/store/types.js
@@ -28,6 +28,7 @@ import { UserData } from '../pages/settings/system/users/new-user/components/use
  * @property {number} id - The id of the user.
  * @property {string} username - The username of the user.
  * @property {number} perfilId - The profile ID associated with the user data.
+ * @property {number} roleId - The role ID associated with the user data.
  * @property {string} createdAt - The creation timestamp.
  * @property {Profile} perfil - The profile details associated with the clinic.
  * @property {string} token - The profile details associated with the clinic.
